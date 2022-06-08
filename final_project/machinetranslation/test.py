1562db83e5d3d2d79bfc34989cdea39c5c87f798
K Hess <khess013@gmail.com>
2022-06-08 15:21:31 -0400

    update 3

--- final_project/machinetranslation/test.py
+++ final_project/machinetranslation/test.py
@@ -4,7 +4,6 @@ import translator
 
 class Test(unittest.TestCase):
 
-
     def test_null_eng_to_fr(self):
         """Null input english to french"""
         self.assertIsNone(translator.englishToFrench(''))
@@ -20,7 +19,6 @@ class Test(unittest.TestCase):
     def test_fr_to_eng(self):
         """Fr-Eng Bonjour"""
         self.assertEqual('Hello', translator.frenchToEnglish('Bonjour'))
-    '''
 
 if __name__ == '__main__':
     unittest.main()
