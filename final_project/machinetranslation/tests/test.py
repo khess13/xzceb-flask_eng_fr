7c0311488199e55001951459c1a44500b10cea5c
K Hess <khess013@gmail.com>
2022-06-08 15:33:48 -0400

    midway

--- final_project/machinetranslation/tests/test.py
+++ final_project/machinetranslation/tests/test.py
@@ -6,19 +6,20 @@ class Test(unittest.TestCase):
 
     def test_null_eng_to_fr(self):
         """Null input english to french"""
-        self.assertIsNone(translator.englishToFrench(''))
+        self.assertIsNone(translator.english_to_french(''))
 
     def test_null_fr_to_eng(self):
         """Null input french to english"""
-        self.assertIsNone(translator.frenchToEnglish(''))
+        self.assertIsNone(translator.french_to_english(''))
 
     def test_eng_to_fr(self):
         """Eng-Fr Hello"""
-        self.assertEqual('Bonjour', translator.englishToFrench('Hello'))
+        self.assertEqual('Bonjour', translator.english_to_french('Hello'))
 
     def test_fr_to_eng(self):
         """Fr-Eng Bonjour"""
-        self.assertEqual('Hello', translator.frenchToEnglish('Bonjour'))
+        self.assertEqual('Hello', translator.french_to_english('Bonjour'))
+
 
 if __name__ == '__main__':
     unittest.main()
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,7 +1,7 @@
-import json
+"""French <> English Translator"""
+import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -16,23 +16,25 @@ translator = LanguageTranslatorV3(
 translator.set_service_url(url)
 
 
-def englishToFrench(englishText):
-    if len(englishText) == 0:
+def english_to_french(english_text):
+    """Translate Enlgish to French"""
+    if len(english_text) == 0:
         return None
     text = translator.translate(
-        englishText,
+        english_text,
         model_id='en-fr'
     ).get_result()
-    frenchText = text['translations'][0].get('translation')
-    return frenchText
+    french_text = text['translations'][0].get('translation')
+    return french_text
 
 
-def frenchToEnglish(frenchText):
-    if len(frenchText) == 0:
+def french_to_english(french_text):
+    """Translate French to English"""
+    if len(french_text) == 0:
         return None
     text = translator.translate(
-        frenchText,
+        french_text,
         model_id='fr-en'
     ).get_result()
-    englishText = text['translations'][0].get('translation')
-    return englishText
+    english_text = text['translations'][0].get('translation')
+    return english_text
